5cce83769a1575a7cab31bcfcacd15681fca1605
Ale <a.bertolo2@campus.unimib.it>
2021-01-28 14:48:52 +0100

    .

--- main.py
+++ main.py
@@ -3,24 +3,51 @@ import numpy as np
 import pandas as pd
 pd.set_option('display.max_columns', None)
 
+import pickle
+import plotly.express as px 
+
 from utils.utils import *
 
 
-### Caricamento dati
+
+"""
+
+#### Caricamento dati
+## Caricamento datasets
 pfizer = pd.read_csv('datasets/vaccination_tweets.csv', parse_dates=[4,9], infer_datetime_format=True)
 vacc = pd.read_csv('datasets/covidvaccine.csv', parse_dates=[3,8], infer_datetime_format=True)
 
-# Join dei tweets
+## Join dei dataset
 comb = pd.concat([pfizer[vacc.columns], vacc], ignore_index=True)
-# Rimozione duplicati basandosi su text, user_name e date
+
+## Rimozione eventuali duplicati basandosi su text, user_name e date
 data = comb.drop_duplicates(subset=['user_name','date','text'], keep='first', ignore_index=True)
 
-### Data engineering
-# Standardizzazione locations
-data['Country'] = get_countries(data.user_location)
+#### Analisi preliminare dati raw
+data.head()
+data.shape
+data.dtypes
+data.info()
 
-### EDA
+#### Data cleaning
+## Gestione valori nulli
+data.isnull().sum()
+#px.imshow(data.isnull()).show() # Visualizzazione heatmap nulli
+data = data.dropna(subset=['user_name'])
+data = data.fillna('None')
 
+## Standardizzazione locations
+data['country'] = get_countries(data['user_location'])
 
+## Salvataggio dati
+data.to_pickle('./bkp/data.pkl')
+
+"""
+#### EDA
+data = pd.read_pickle("data.pkl")
 
 import pdb; pdb.set_trace()
+
+
+
+#### Sentiment Analysis
--- utils/utils.py
+++ utils/utils.py
@@ -1,10 +1,8 @@
 import pandas as pd
 import string
-import nltk
 import re
-from nltk.tokenize import word_tokenize
+import nltk
 from nltk.tokenize import WordPunctTokenizer
-#nltk.download('punkt')
 
 def deEmojify(text):
     regrex_pattern = re.compile(pattern = "["
@@ -15,114 +13,98 @@ def deEmojify(text):
                            "]+", flags = re.UNICODE)
     return regrex_pattern.sub(r'',text)
 
-def preproc(text):
-    new_text = re.sub(r'http\S+', '', text) #urls
-    new_text = re.sub(r'@[^\s]+', '', new_text) #mentions
-    #new_text = new_text.lower() #lowercase
-    new_text = re.sub(r'—|’|’’|-|”|“|‘', ' ', new_text) #separators and quotes
-    new_text = new_text.strip()
-    new_text = re.sub(r'\d+', '', new_text) #digits
-    new_text = new_text.translate(str.maketrans('', '', string.punctuation)) #punteggiatura
-    new_text = deEmojify(new_text)
-    new_text = WordPunctTokenizer().tokenize(new_text)
-    return new_text
-
-
-def get_country(row):
-    tmp = []
-    #import pdb; pdb.set_trace()
-    countries,cities = get_codes()
-
-    import pdb; pdb.set_trace()
-    if countries.isin(row).any().any():
-        yep = [i for i, x in enumerate(countries.isin(row).any()) if x][0]
-        tmp = countries[countries.iloc[:,yep]==token]['code3'].values[0]
-
-
-
-    for idx, token in enumerate(row):
-        # Se trova il codice a 2 o 3 char, o il country name salta gli altri token
-        # if token in countries['code2'].tolist():
-        #     tmp = (countries[countries['code2']==token]['code3'].values[0])
-        #     break
-        # elif token in countries['code3'].tolist():
-        #     tmp = token
-        #     break
-        # elif token in countries['country'].tolist():
-        #     tmp = token
-        #     break
-        # Cerca anche nei nomi alternativi dei paesi
-        #if False:
-        #    print(x)
-        #else:
-        #    if countries.isin([token]).any().any():
-        #        yep = [i for i, x in enumerate(countries.isin([token]).any()) if x][0]
-        #        tmp = countries[countries.iloc[:,yep]==token]['code3'].values[0]
-        #        break
-                #for idx,val in enumerate(countries.isin([token]).any()):
-                #    if val:
-                #        import pdb; pdb.set_trace()
-                #        tmp = countries[countries.iloc[:,idx]==token]['code3'].values[0]
-                #        break
-
-
-        # Se trova il nome di una città, aggiungi per la valutazione
-        if token in cities['name'].values:
-            #import pdb; pdb.set_trace()
-            tmp_idx = cities[cities['name']==token]['country'].index.values
-            #import pdb; pdb.set_trace()
-            if len(tmp_idx) > 1 and idx+1<len(row): # trovate città omonime, guardo il token successivo
-                if row[idx+1] in cities.iloc[tmp_idx]['country'].values:
-                    tmp2 = cities.iloc[tmp_idx][cities.iloc[tmp_idx]['country']==row[idx+1]]
-                    tmp = tmp2['country'].values[0]
-                    break
-                elif row[idx+1] in cities.iloc[tmp_idx]['subcountry'].values:
-                    tmp2 = cities.iloc[tmp_idx][cities.iloc[tmp_idx]['subcountry']==row[idx+1]]
-                    tmp = tmp2['country'].values[0]
-                    break
-            else:
-                tmp_country = cities.iloc[tmp_idx]['country'].values[0]
-                if countries.isin([tmp_country]).any().any():
-                    yep = [i for i, x in enumerate(countries.isin([tmp_country]).any()) if x][0]
-                    import pdb; pdb.set_trace()
-                    tmp = countries[countries.iloc[:,yep]==tmp_country]['code3'].values[0]
-                    break
-
-
-    if len(tmp) <1:
-        tmp = None
-
-    print(row, '->', str(tmp))
-    return tmp
-
-
-
-def get_codes():
-    cities = pd.read_csv('./utils/world-cities.csv')
-    countries_raw = pd.read_csv('./utils/country_iso_codes_expanded.csv')
-    countries = countries_raw.drop(countries_raw.columns[4],axis=1)
-    #countries = countries_raw.drop(countries_raw.columns[3:],axis=1)
-    #countries['alternatives'] = countries_raw.iloc[:,5:].values.tolist()
-    # remove nan
-    #countries['alternatives'] = countries.alternatives.apply(lambda row: [x for x in row if str(x) != 'nan'])
-    countries.fillna('None')
-    countries.rename(columns={'Alpha-2 code': 'code2', 'Alpha-3 code': 'code3'}, inplace=True)
-
-    return countries, cities
-
-
 
 def get_countries(df):
+    '''
+    Processa la colonna locations e restituisce la standardizzazione (3-char code)
+    '''
 
-    # Dizionario
-    #countries,cities = get_codes()
-    #import pdb; pdb.set_trace()
+    def get_codes():
+        '''
+        Carica i dizionari utilizzati per la standardizzazione della locations
+        '''
+        cities = pd.read_csv('./utils/world-cities.csv')
+        usa_states = pd.read_csv('./utils/usa-countries-code.csv')
+        countries_raw = pd.read_csv('./utils/country_iso_codes_expanded.csv')
+        countries = countries_raw.drop(countries_raw.columns[4],axis=1)
+        countries.fillna('None')
+        countries.rename(columns={'Alpha-2 code': 'code2', 'Alpha-3 code': 'code3'}, inplace=True)
+        return countries, cities, usa_states
 
+    def loc_preprocess(text):
+        '''
+        Custom tokenizer per il campo location
+        '''           
+        new_text = re.sub(r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', '', text) #urls
+        new_text = re.sub(r'(?:@[\w_]+)', '', new_text) #mentions
+        new_text = re.sub(r'(?:\#+[\w_]+[\w\'_\-]*[\w_]+)', '', new_text) # hash-tags
+        new_text = re.sub(r'—|’|’’|-|”|“|‘', ' ', new_text) #separators and quotes
+        new_text = new_text.strip()
+        new_text = re.sub(r'(?:(?:\d+,?)+(?:\.?\d+)?)', '', new_text) #numbers
+        new_text = new_text.translate(str.maketrans('', '', string.punctuation)) #punteggiatura
+        new_text = deEmojify(new_text)
+        new_text = WordPunctTokenizer().tokenize(new_text)
+        return new_text
 
+    def get_country(row, countries, cities, usa):
+        '''
+        Per ciascuna location pre-processata, effettua la standardizzazione (3-char)
+        '''
+        tmp = []
+        if not row or row[0]=='None':
+            return None
 
-    df = df.fillna('None')
-    good = df.apply(preproc).apply(get_country)
+        for idx, token in enumerate(row):
+            # Se trova il codice a 2 o 3 char, il nome o i nomi alternativi di uno stato del mondo
+            if token in countries['code2'].tolist():
+                tmp = (countries[countries['code2']==token]['code3'].values[0])
+                break
+            elif token in countries['code3'].tolist():
+                tmp = token
+                break
+            elif token in countries['country'].tolist():
+                tmp = countries[countries['country']==token]['code3'].values[0]
+                break
+            elif countries.iloc[:,4:].isin([token]).any().any():
+                column = [i for i, x in enumerate(countries.iloc[:,4:].isin([token]).any()) if x][0]
+                tmp = countries[countries.iloc[:, column+4]==token]['code3'].values[0]
+                break
 
-    #df.iloc[14:20][0].apply(preproc).apply(get_country)
+            # Se trova il nome di una città
+            if len(token) > 4 and token in cities['name'].values:
+                tmp_idx = cities[cities['name']==token]['country'].index.values
+                tmp_country = []
+                if len(tmp_idx) > 1 and idx+1<len(row): # trovate città omonime
+                    # guarda il token successivo
+                    if row[idx+1] in cities.iloc[tmp_idx]['country'].values:
+                        tmp2 = cities.iloc[tmp_idx][cities.iloc[tmp_idx]['country']==row[idx+1]]
+                        tmp_country = tmp2['country'].values[0]
+                    elif row[idx+1] in cities.iloc[tmp_idx]['subcountry'].values:
+                        tmp2 = cities.iloc[tmp_idx][cities.iloc[tmp_idx]['subcountry']==row[idx+1]]
+                        tmp_country = tmp2['country'].values[0]
+                else: # trovata una sola città
+                    tmp_country = cities.iloc[tmp_idx]['country'].values[0]
+
+                # estrae il codice dalla città trovata
+                if countries.isin([tmp_country]).any().any():
+                    column = [i for i, x in enumerate(countries.isin([tmp_country]).any()) if x][0]
+                    tmp = countries[countries.iloc[:,column]==tmp_country]['code3'].values[0]
+                    break
+
+            # Se trova il nome di uno stato USA
+            if token in usa['State'].values or token in usa['Abbreviation'].values:
+                tmp = 'USA'
+        
+        # Nessuna corrispondenza geografica trovata
+        if len(tmp)<1:
+            tmp = None
+
+        print(row, '->', str(tmp))
+        return tmp
+
     
-    #good = df[0].apply(preproc).apply(get_country)
+
+    # Caricamento dizionario dei paesi, città e stati usa
+    countries, cities, usa_states = get_codes()
+    countries_codes = df.apply(loc_preprocess).apply(get_country, args=(countries,cities,usa_states))
+    return countries_codes
